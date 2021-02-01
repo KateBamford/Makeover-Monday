af7feab9c55ed160c5c6b5f9aee05d764b765bed
KateBamford <kate_bamford@yahoo.com>
2021-02-01 21:46:50 +0000

    Add patchwork

--- Week_4_makeover_monday.R
+++ Week_4_makeover_monday.R
@@ -17,8 +17,9 @@ citaion_copyright <- data.world::query(citaion_copyright_sql, data_world)
 citaion_copyright
 mines_datasheet
 
+background.colour <- "#f1f1f1"
 
-mines_datasheet %>%
+chart <- mines_datasheet %>%
   group_by(district_name) %>%
   summarise(number_mines = n(), 
             total_coal_production_mt = sum(coal_lignite_production_mt_2019_2020)) %>%
@@ -28,20 +29,28 @@ mines_datasheet %>%
   ggplot(aes(x = factor(district_name, levels = unique(district_name)), 
              y = total_coal_production_mt, fill = number_mines)) + 
              geom_bar(stat = 'identity') +
-             geom_point(aes(y = number_mines), color = "#fac127", size = 2)  +   
+             geom_point(aes(y = number_mines, colour = ""), size = 2.5)  +  
+             scale_colour_manual(values = "#fac127", name = "Number of mines") + 
              scale_fill_gradient2(name = "Number of mines",
                                   high = "#280b54", mid = "#9f8eb0", low = "#c7bed0", limits = c(0,70)) +
              scale_y_continuous(name = "Total coal production (MT)") +
              scale_x_discrete(name = "District") +
            theme_bw() +
            theme(
-             axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
-             #axis.title = element_blank(),
+             #axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+             panel.border = element_blank(),
+             axis.line = element_line(colour = "black"),
              panel.grid = element_blank(),
-             legend.position = "bottom"
-           ) +
+             legend.position = "bottom",
+             plot.background = element_rect(fill = background.colour),
+             panel.background = element_rect(fill= background.colour, color = background.colour),
+             legend.background = element_blank(),
+             legend.box.background = element_blank(),
+             legend.key = element_rect(fill = "transparent", colour = "transparent"),
+             axis.text = element_text(colour = "#280b54")) +
   coord_flip()
            
+
 
 # Get the shape file of India
 data(wrld_simpl)
@@ -68,7 +77,7 @@ mines_datasheet %>%
   distinct()
 
 
-ggplot(data = india_map) +
+map <- ggplot(data = india_map) +
   geom_polygon(aes( x = long, y = lat, group = group), 
                size = 0.5, alpha = 0.5, fill = "#6b5286", colour = "white") + 
   geom_point(data = mines, aes(x = longitude, y = latitude, 
@@ -80,6 +89,30 @@ ggplot(data = india_map) +
         panel.grid = element_blank(),
         axis.ticks = element_blank(),
         axis.text = element_blank(),
-        axis.title = element_blank()) + 
+        axis.title = element_blank(),
+        plot.background = element_rect(fill = background.colour),
+        panel.background = element_rect(fill= background.colour, color = background.colour),
+        legend.background = element_rect(fill= background.colour, color = background.colour),
+        legend.position = "bottom",
+        legend.key = element_rect(fill = "transparent", colour = "transparent")) + 
   coord_map()
   
+# Patchwork 
+
+layout <- c(
+  area(t = 2, l = 1, b = 5, r = 4),
+  area(t = 1, l = 4, b = 3, r = 6)
+)
+
+patchwork <- chart + map + plot_layout(guides = 'collect') +
+  plot_layout(design = layout) 
+
+patchwork + plot_annotation(
+  title = 'Indian Coal Mine Location and Production',
+  caption = paste0('Data source:\nRSandeep Pai and Hisham Zerriffi.\nA novel dataset for analysing sub-national socioeconomic developments\nin the Indian coal industry, IOPSciNotes,\nhttps://doi.org/10.1088/2633-1357/abdbbb')
+) & 
+  theme(text = element_text('mono', colour = '#280b54'), 
+        plot.title = element_text(size = 25),
+        plot.background = element_rect(fill = background.colour, colour = background.colour))
+
+
